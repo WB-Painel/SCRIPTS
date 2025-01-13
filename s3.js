9a9a8f1f44f2a8354aef4ede0f7efeb2107f15e6
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2025-01-12 23:02:14 -0300

    Create s3.js

---
+++ s3.js
@@ -0,0 +1,25 @@
+var div = document.createElement("center");
+	
+	div.id = "DownloadBlock";
+	
+	div.style.backgroundColor = "#181818ff";
+	
+	div.style.justifyContent="center";
+	
+	div.style.alignItems="center";
+	
+	div.style.width=window.innerWidth + "px";
+	
+	div.style.height=window.innerHeight + "px";
+	
+	div.style.position="fixed";
+	
+	div.style.left="0";
+	
+	div.style.top="0";
+	
+	div.style.right="0";
+	
+	div.style.bottom="0";
+	
+	document.body.appendChild(div);

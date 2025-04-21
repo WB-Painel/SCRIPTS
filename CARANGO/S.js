9c4173c1ac92febd99fd9a9aaf266cda77b08c99
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2025-04-21 15:51:02 -0300

    Update S.js

--- CARANGO/S.js
+++ CARANGO/S.js
@@ -755,11 +755,10 @@ var ViewOne = false;
 	}
 	
 	function AdTwo(){
-
 	var Content2_slot;
-  googletag.cmd.push(function() {
-  var mapping =	googletag.sizeMapping().addSize([0, 0], ['fluid',[250, 250],[300, 250],[336, 280]]).build();
-  Content2_slot = googletag.defineSlot("/23275015135/carangoweb/carangoweb_Content2",[[250, 250],[300, 250],[336, 280]], "Content2")
+	googletag.cmd.push(function() {
+	var mapping =	googletag.sizeMapping().addSize([0, 0], ['fluid',[250, 250],[300, 250],[336, 280]]).build();
+	Content2_slot = googletag.defineSlot("/23275015135/carangoweb/carangoweb_Content2",[[250, 250],[300, 250],[336, 280]], "Content2")
 	.defineSizeMapping(mapping)
 	.setCollapseEmptyDiv(true)
 	.addService(googletag.pubads());
@@ -779,21 +778,14 @@ var ViewOne = false;
 	}
 	
 	function AdTree(){
-
 	var Content3_slot;
 	googletag.cmd.push(function() {
 	var mapping = 	googletag.sizeMapping().addSize([0, 0], ['fluid',[250, 250],[300, 250],[336, 280]]).build();
-	Content3_slot = googletag.defineSlot("/22967751971/equipeatualizada/equipeatualizada_Content3",[[250, 250],[300, 250],[336, 280]], "Content3")
+	Content3_slot = googletag.defineSlot("/23275015135/carangoweb/carangoweb_Content3",[[250, 250],[300, 250],[336, 280]], "Content3")
 	.defineSizeMapping(mapping)
 	.setCollapseEmptyDiv(true)
 	.addService(googletag.pubads());
-	/*googletag.pubads().enableLazyLoad({
-	fetchMarginPercent: 20,
-	renderMarginPercent: 10,
-	mobileScaling: 2.0
-        });*/
 	googletag.enableServices();
-
 });
 	
 	var AdTree = document.createElement("div");
